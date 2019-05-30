02ba1ca5bd1f48ea4adc1a1d5c1d6624154c7b81
Joshua D. Forrest <joshuadforrest@gmail.com>
2019-05-30 19:50:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,5 +21,15 @@ function appendDriver (name) {
   return [...drivers, name];
 }
 
+function prependDriver (name) {
+  return [name, ...drivers];
+}
 
+function removeFirstDriver () {
+  return drivers.slice(1);
+}
+
+function removeLastDriver () {
+  return drivers.slice(0, drivers.length - 1);
+}
 
